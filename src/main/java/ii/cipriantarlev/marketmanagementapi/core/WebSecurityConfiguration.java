57886fa642bf7c235123930e917407b7341d8715
cipriantarlev <ciprian.tarlev@gmail.com>
2021-06-15 15:09:22 +0300

    MM-11: modified login service in order to connect to ui

--- src/main/java/ii/cipriantarlev/marketmanagementapi/core/WebSecurityConfiguration.java
+++ src/main/java/ii/cipriantarlev/marketmanagementapi/core/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package ii.cipriantarlev.marketmanagementapi.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,9 +28,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http
+		.authorizeRequests()
+		.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 				.anyRequest().authenticated()
 			.and()
-			.httpBasic();
+				.httpBasic();
 	}
 }
