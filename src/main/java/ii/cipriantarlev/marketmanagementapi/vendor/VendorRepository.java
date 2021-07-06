767a70340bfd34605fd0b70d581ff6ed7d007a5d
cipriantarlev <ciprian.tarlev@gmail.com>
2021-07-06 14:44:07 +0300

    MM-19: Added ordered by id fetch for vendors

--- src/main/java/ii/cipriantarlev/marketmanagementapi/vendor/VendorRepository.java
+++ src/main/java/ii/cipriantarlev/marketmanagementapi/vendor/VendorRepository.java
@@ -1,7 +1,10 @@
 package ii.cipriantarlev.marketmanagementapi.vendor;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VendorRepository extends JpaRepository<Vendor, Integer> {
 
+	public List<Vendor> findAllByOrderByIdAsc();
 }
