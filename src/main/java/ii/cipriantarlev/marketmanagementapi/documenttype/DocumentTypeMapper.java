a0b428df2821c57f9f17b725431bdf02fa8d52ca
cipriantarlev <ciprian.tarlev@gmail.com>
2021-08-05 17:54:14 +0300

    MM-39: Created Document Type Rest Controller

---
+++ src/main/java/ii/cipriantarlev/marketmanagementapi/documenttype/DocumentTypeMapper.java
@@ -0,0 +1,20 @@
+package ii.cipriantarlev.marketmanagementapi.documenttype;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DocumentTypeMapper {
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	public DocumentTypeDTO mapEntityToDTO(DocumentType docymentType) {
+		return modelMapper.map(docymentType, DocumentTypeDTO.class);
+	}
+
+	public DocumentType mapDTOToEntity(DocumentTypeDTO docymentTypeDTO) {
+		return modelMapper.map(docymentTypeDTO, DocumentType.class);
+	}
+}
---
+++ src/main/java/ii/cipriantarlev/marketmanagementapi/documenttype/DocumentTypeRepository.java
@@ -0,0 +1,7 @@
+package ii.cipriantarlev.marketmanagementapi.documenttype;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocumentTypeRepository extends JpaRepository<DocumentType, Integer> {
+
+}
