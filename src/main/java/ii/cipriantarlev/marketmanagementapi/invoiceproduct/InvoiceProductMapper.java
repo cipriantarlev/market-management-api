000bfa93302fc06a6bb0fc0664f3f5ed25913724
cipriantarlev <ciprian.tarlev@gmail.com>
2021-08-12 13:25:29 +0300

    MM-41: Created Invoice Products RestController

---
+++ src/main/java/ii/cipriantarlev/marketmanagementapi/invoiceproduct/InvoiceProductMapper.java
@@ -0,0 +1,20 @@
+package ii.cipriantarlev.marketmanagementapi.invoiceproduct;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InvoiceProductMapper {
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	public InvoiceProductDTO mapEntityToDTO(InvoiceProduct invoiceProduct) {
+		return modelMapper.map(invoiceProduct, InvoiceProductDTO.class);
+	}
+
+	public InvoiceProduct mapDTOToEntity(InvoiceProductDTO invoiceProductDTO) {
+		return modelMapper.map(invoiceProductDTO, InvoiceProduct.class);
+	}
+}
--- src/main/java/ii/cipriantarlev/marketmanagementapi/product/ProductRepository.java
+++ src/main/java/ii/cipriantarlev/marketmanagementapi/product/ProductRepository.java
@@ -1,7 +1,10 @@
 package ii.cipriantarlev.marketmanagementapi.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	@Query(value = "select p.id, name_rom, name_rus, category_id, subcategory_id, discount_price, retail_price, trade_margin, measuring_unit_id, vat_id, plu_id, product_code_id, stock from public.products as p left join barcodes as b on b.product_id = p.id where b.value = ?1", nativeQuery = true)
+	Product findByBarcodeValue(String barcodeValue);
 }
