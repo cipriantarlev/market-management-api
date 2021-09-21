c7e6ec1602fc9bf7272b8d1d738d8a08afa9375f
cipriantarlev <ciprian.tarlev@gmail.com>
2021-09-21 13:21:34 +0300

    MM-47: Added exception handler for UniqueConstraintViolation

--- src/main/java/ii/cipriantarlev/marketmanagementapi/error/GlobalExceptionHandler.java
+++ src/main/java/ii/cipriantarlev/marketmanagementapi/error/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -31,12 +33,32 @@ public class GlobalExceptionHandler {
 					.status(HttpStatus.BAD_REQUEST.value())
 					.field(((FieldError) error).getField())
 					.message(error.getDefaultMessage())
-					.timeStamp(LocalDateTime.now())
-					.build();
+					.timeStamp(LocalDateTime.now()).build();
+			
 			log.error("Validation not passed for '{}' for field '{}' with message: {}", error.getObjectName(),
 					errorResponse.getField(), errorResponse.getMessage());
 			validationErrorList.add(errorResponse);
 		});
 		return new ResponseEntity<>(validationErrorList, HttpStatus.BAD_REQUEST);
 	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<ValidationErrorResponse> handleUniqueConstraintViolation(
+			DataIntegrityViolationException exception) {		
+		
+		var rootCauseMessage = exception.getRootCause() != null ? exception.getRootCause().getMessage() : exception.getMessage();
+		
+		var fieldMessage = StringUtils.substringAfterLast(rootCauseMessage, '\n').trim();
+		var errorMessage = StringUtils.substringBeforeLast(rootCauseMessage, "\n").replace("\"", "'");
+		
+			ValidationErrorResponse errorResponse = ValidationErrorResponse.builder()
+					.status(HttpStatus.BAD_REQUEST.value())
+					.field(fieldMessage)
+					.message(errorMessage)
+					.timeStamp(LocalDateTime.now())
+					.build();
+			log.error("Unique constraint violation error: {}", errorResponse);
+
+		return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
+}
 }
